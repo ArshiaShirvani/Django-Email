ef358fa0b36b831e41e859d1b70b6c9d527830ed
Arshia <arshiashirvani.f1385@gmail.com>
2025-03-31 14:38:28 +0330

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -128,17 +128,17 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#account settings
+#a ccount settings
 AUTH_USER_MODEL = 'accounts.User'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-#email settings
+# email settings
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # یا سرور SMTP دیگر
+EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'arshiadjango@gmail.com'  # ایمیل فرستنده
-EMAIL_HOST_PASSWORD = 'pvldxclhtdkbxnyj'  # رمز عبور ایمیل فرستنده
+EMAIL_HOST_USER = 'EMAIL SERVER'  
+EMAIL_HOST_PASSWORD = 'PASSWORD EMAIL'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
